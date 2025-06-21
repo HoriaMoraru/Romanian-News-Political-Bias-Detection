2df9b16e81b5d1c612a3db6e5baf28f39ecabd71
HoriaMoraru <horia.moraru14@gmail.com>
2025-06-21 19:58:25 +0300

    fixed weighted loss bug.

--- model/deeplearn/WeightedLossTrainer.py
+++ model/deeplearn/WeightedLossTrainer.py
@@ -1,12 +1,16 @@
 from transformers import Trainer
-import torch.nn.functional as F
+import torch
 
 class WeightedLossTrainer(Trainer):
-    def compute_loss(self, model, inputs, return_outputs=False):
+    def compute_loss(self, model, inputs, return_outputs=False, **kwargs):
         labels = inputs.pop("labels")
         weights = inputs.pop("weight")
+
         outputs = model(**inputs)
         logits = outputs.logits
-        loss = F.cross_entropy(logits, labels, reduction='none')
-        weighted_loss = (loss * weights).mean()
+
+        loss_fn = torch.nn.CrossEntropyLoss(reduction="none")
+        losses = loss_fn(logits, labels)
+        weighted_loss = torch.mean(losses * weights)
+
         return (weighted_loss, outputs) if return_outputs else weighted_loss
