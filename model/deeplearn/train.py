dfc23542046f711dddac4872ebbd80cdaec85056
HoriaMoraru <horia.moraru14@gmail.com>
2025-06-23 12:02:53 +0300

    adjusted lr.

--- model/deeplearn/train.py
+++ model/deeplearn/train.py
@@ -97,11 +97,10 @@ def main():
     model = AutoModelForSequenceClassification.from_pretrained(MODEL_NAME, num_labels=NUM_LABELS)
 
     args = TrainingArguments(
-        learning_rate=8e-6,
+        learning_rate=2e-6,
         lr_scheduler_type="linear",
         optim="adamw_torch",
-        num_train_epochs=7,
-        max_grad_norm=0.5,
+        num_train_epochs=12,
         label_names=["labels"],
         eval_strategy="epoch",
         save_strategy="epoch",
