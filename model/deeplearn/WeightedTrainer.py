dcabc7a28211a1ada28f1a04a9a6e4c8e2d7fa50
HoriaMoraru <horia.moraru14@gmail.com>
2025-06-23 11:48:26 +0300

    changed weights.

--- model/deeplearn/WeightedTrainer.py
+++ model/deeplearn/WeightedTrainer.py
@@ -9,7 +9,7 @@ class WeightedTrainer(Trainer):
         outputs = model(**inputs)
         logits = outputs.get('logits')
 
-        loss_fct = nn.CrossEntropyLoss(weight=torch.tensor([1.267, 0.825], device=logits.device))
+        loss_fct = nn.CrossEntropyLoss(weight=torch.tensor([1.268, 0.825], device=logits.device))
 
         loss = loss_fct(logits.view(-1, self.model.config.num_labels), labels.view(-1))
         return (loss, outputs) if return_outputs else loss
